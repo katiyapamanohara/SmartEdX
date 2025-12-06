8033b1fb02be8cb2573d4507d96ef240f89a165a
yohanmanohara <warushayohan80@gmail.com>
2025-12-06 21:50:17 +0530

    Refactor code structure for improved readability and maintainability

---
+++ frontend/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"idDQ20BgIfVOtaGxEcrapdsAHqqB04Ymx5vsz20swjk=\"\n}"
---
+++ frontend/.next/dev/static/chunks/src_app_layout_tsx_78cdd4a3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__49a648de._.css",
+  "static/chunks/_9afd288f._.js"
+],
+    source: "dynamic"
+});
